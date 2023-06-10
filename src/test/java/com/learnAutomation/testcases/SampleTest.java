0ef0adcfb7a9e7b1e4069b7c8270b88b3d11e0b1
Banasri <banasrisaikia30@gmail.com>
2023-06-10 22:35:22 +0530

    mess

---
+++ src/test/java/com/learnAutomation/testcases/SampleTest.java
@@ -0,0 +1,10 @@
+package com.learnAutomation.testcases;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void helloTest() {
+		System.out.println("Sample test executed");
+	}
+}
